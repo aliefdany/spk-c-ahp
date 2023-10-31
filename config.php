d67794f8b3a340126de7f8dd940f51d673cc85b6
aliefdany <works@alief.tech>
2023-11-01 01:11:08 +0700

    hardcoded env for db

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
-	// connection
-	$host = 'localhost';
-	$username = 'root';
-	$password = '';
-	$database = 'ahp';
-
-	$koneksi = mysqli_connect($host,$username,$password);
+	$koneksi = mysqli_init();
+	$koneksi->ssl_set(NULL, NULL, "/etc/pki/tls/certs/ca-bundle.crt", NULL, NULL);
+	$koneksi->real_connect("aws.connect.psdb.cloud", "piso5jah08z4je5u8xg0", "pscale_pw_6fgJNzP76qDzr5wt9wDK4GyFJqxemYxA9KeIPYH7Gnk", "ahp");
+	// $mysqli->close();
 
 	if (!$koneksi)
 	{
@@ -13,7 +10,7 @@
 		exit();
 	}
 
-	if(!mysqli_select_db($koneksi, $database))
+	if(!mysqli_select_db($koneksi, "ahp"))
 	{
 		echo "Tidak dapat menemukan database";
 		exit();
