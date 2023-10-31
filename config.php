9e409c83614079795359562c1fffe250c01a58cf
aliefdany <works@alief.tech>
2023-11-01 01:15:08 +0700

    updated ca cert root

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	$koneksi = mysqli_init();
-	$koneksi->ssl_set(NULL, NULL, "/etc/pki/tls/certs/ca-bundle.crt", NULL, NULL);
+	$koneksi->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 	$koneksi->real_connect("aws.connect.psdb.cloud", "piso5jah08z4je5u8xg0", "pscale_pw_6fgJNzP76qDzr5wt9wDK4GyFJqxemYxA9KeIPYH7Gnk", "ahp");
 	// $mysqli->close();
 
